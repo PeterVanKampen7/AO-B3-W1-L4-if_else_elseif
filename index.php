b5f7fa064cb32542275a31253064796417df34cf
Peter <99065921@mydavinci.nl>
2021-02-15 11:42:00 +0100

    Done

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+    date_default_timezone_set('Europe/Amsterdam');
+    $time = date("H:i"); 
+    $hour = date("H");
+    $greetString;
+    $imageURL;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AO-B3-W1-L4-if_else_elseif</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Grand+Hotel&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        if($hour >= 18){
+            $greetString = "Goede avond";
+            $imageURL = "evening.png";
+        } else if($hour >= 12){
+            $greetString = "Goede middag";
+            $imageURL = "afternoon.png";
+        } else if($hour >= 6){
+            $greetString = "Goede morgen";
+            $imageURL = "morning.png";
+        } else {
+            $greetString = "Goede nacht";
+            $imageURL = "night.png";
+        }
+    ?>
+    <style>
+        body{
+            background-image: url("images/<?php echo $imageURL; ?>");
+        }
+    </style>
+    <div id="container">
+        <h1><?php echo $greetString; ?></h1>
+        <br>
+        <h1>Het is nu <?php echo $time; ?></h1>
+    </div>
+</body>
+</html>
